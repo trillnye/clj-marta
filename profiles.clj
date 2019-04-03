d97e24a9ef24ad8a315dda86f3545db5213de202
Chris Smith <chris.smith@careerbuilder.com>
2019-04-02 23:31:13 -0400

    added dev environment

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,8 @@
-{:repl {:env {:marta-api-key "475ad2ba-5928-4063-9d00-ae06fbb02f3c"
+{:repl {:env {:marta-api-key "some-key-here"
               :marta-api-uri "http://developer.itsmarta.com"}
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
 {:dev {:resource-paths ["test/resources"]
        :plugins [[codox "0.10.0"]]
        :codox {:source-paths ["src/clojure"]}
-       :env {:marta-api-key "475ad2ba-5928-4063-9d00-ae06fbb02f3c"
+       :env {:marta-api-key "some-key-here"
              :marta-api-uri "http://developer.itsmarta.com"}}}
