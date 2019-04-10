4e4496ab246a8b5135bd6a23673da73143c06957
Chris Smith <chris.smith@careerbuilder.com>
2019-04-10 11:47:51 -0400

    Excise unneeded repl dependencies

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,8 @@
-{:repl {:env {:marta-api-key "475ad2ba-5928-4063-9d00-ae06fbb02f3c"
+{:repl {:env {:marta-api-key "some-key-here"
               :marta-api-uri "http://developer.itsmarta.com"}
-        :dependencies [[org.clojure/tools.nrepl "0.2.13"]
-                       [org.clojure/data.csv "0.1.4"]
-                       [camel-snake-kebab "0.4.0"]]}}
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
 {:dev {:resource-paths ["test/resources"]
        :plugins [[codox "0.10.0"]]
        :codox {:source-paths ["src/clojure"]}
-       :env {:marta-api-key "475ad2ba-5928-4063-9d00-ae06fbb02f3c"
+       :env {:marta-api-key "some-key-here"
              :marta-api-uri "http://developer.itsmarta.com"}}}
