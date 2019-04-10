9806625adfc4e26d0ef8e323097fcb6193eb563b
Chris Smith <chris.smith@careerbuilder.com>
2019-04-10 01:15:07 -0400

    Static schedule parser

--- project.clj
+++ project.clj
@@ -5,23 +5,24 @@
             :url "https://opensource.org/licenses/MIT"
             :distribution :repo
             :comments "MIT license"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [clojurewerkz/support "1.1.0"]
                  [camel-snake-kebab "0.4.0"]
-                 [clj-http "2.3.0"]
-                 [cheshire "5.7.0"]
+                 [clj-http "3.9.1"]
+                 [cheshire "5.8.1"]
                  [environ "1.1.0"]
+                 [clj-time "0.15.0"]
                  [org.clojure/data.csv "0.1.4"]]
-  :profiles {:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.8:dev,master"]}
-  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+  :profiles {:1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
+             :master {:dependencies [[org.clojure/clojure "1.10.0"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.10:dev,master"]}
+  :repositories {"sonatype" {:url "https://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
-                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                 "sonatype-snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :javac-options      ["-target" "1.6" "-source" "1.6"]
+  :javac-options      ["-target" "1.8" "-source" "1.8"]
   :jvm-opts           ["-Dfile.encoding=utf-8"]
   :source-paths       ["src/clojure"]
   :java-source-paths  ["src/java"])
--- src/clojure/itg/clj_marta/analyzer/core.clj
+++ src/clojure/itg/clj_marta/analyzer/core.clj
@@ -6,24 +6,53 @@
             [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
-(def static-schedule-root "data/schedules/")
-(def rail-root "rail/")
-(def bus-root "bus/")
-(def weekday-root "weekday/")
-(def saturday-root "saturday/")
-(def sunday-root "sunday/")
-(def northbound-root "northbound/")
-(def southbound-root "southbound/")
-(def westbound-root "westbound/")
-(def eastbound-root "eastbound/")
+(def rail-file-location "data/schedules/rail/%s/%s/%s.csv")
+
+(def rail-schedules ["weekday" "saturday" "sunday"])
+
+(def rail-lines  {"red" ["northbound" "southbound"]
+                  "gold" ["northbound" "southbound"]
+                  "blue" ["eastbound" "westbound"]
+                  "green" ["eastbound" "westbound"]})
 
 
-(defn load-static-schedule []
-  (with-open [reader (io/reader "data/schedules/rail/weekday/northbound/gold.csv")]
-    (doall
-      (csv/read-csv reader))))
+(defn rotate-matrix [matrix]
+  (apply mapv vector matrix))
 
-(defn get-rail-schedule [station direction]
-  (let [static-schedule (load-static-schedule)] (reduce (fn [acc item]
-             (conj acc {(keyword (get item 0)) (drop 1 item)})
-             ) [] (apply mapv vector static-schedule))))
+(defn get-schedule-file-name [schedule line direction]
+  (format rail-file-location schedule line direction))
+
+(defn load-static-schedule [schedule line direction]
+  (with-open [reader (io/reader  (get-schedule-file-name schedule line direction))]
+    (rotate-matrix
+     (doall
+      (csv/read-csv reader)))))
+
+(defn load-station-arrivals [schedule line direction]
+  (let [static-schedule (load-static-schedule schedule line direction)]
+    (reduce
+     (fn [acc item]
+      (merge acc
+              {
+               :station-name (get item 0)
+               :arrivals (take 5 (drop 1 item))
+              })) [] static-schedule)))
+
+(defn load-directions [schedule line directions]
+    (reduce (fn [acc item]
+              (assoc acc (keyword item) (load-station-arrivals schedule line item))
+              ) {} directions))
+
+(defn load-lines [schedule lines]
+  (reduce (fn [acc item]
+            (assoc acc (keyword item) (load-directions schedule item (rail-lines item)))
+            ) {} lines))
+
+(defn load-schedules []
+  (reduce (fn [acc item]
+            (assoc acc (keyword item) (load-lines item (keys rail-lines)))
+            ) {} rail-schedules))
+
+(comment (defn get-static-schedule-by-station [station-name]
+   (let [stations (parse-static-schedule (load-static-schedule))]
+     (filter #(= (% :station-name) station-name) stations))))
