0e00a72f0cad047709d4d4949e59f44d0d1988c4
Chris Smith <chris.smith@careerbuilder.com>
2019-04-10 15:06:22 -0400

    [WIP] Sample forecaster (#2)
    * [WIP] Sample forecaster
    
    * Update dependencies

--- src/clojure/itg/clj_marta/analyzer/core.clj
+++ src/clojure/itg/clj_marta/analyzer/core.clj
@@ -2,9 +2,9 @@
   (:require [cheshire.core :refer :all]
             [clojure.string :as string]
             [itg.clj-marta.common.util :refer :all]
-            [itg.clj-marta.analyzer.static-schedule :refer :all]
+            [itg.clj-marta.analyzer.static-schedule :as stan]
             [camel-snake-kebab.core :refer :all]
             [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
-(def static-rail-schedule (load-schedule))
+(def static-rail-schedule (stan/load-rail-schedule))
