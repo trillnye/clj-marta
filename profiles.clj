ac215c58677f189cc1347a4ec8c285a0bab096c5
Chris Smith <chris.smith@careerbuilder.com>
2019-04-10 01:30:58 -0400

    Profile updates

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,4 @@
-{:repl {:env {:marta-api-key "475ad2ba-5928-4063-9d00-ae06fbb02f3c"
+{:repl {:env {:marta-api-key "api-key-here"
               :marta-api-uri "http://developer.itsmarta.com"}
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]
                        [org.clojure/data.csv "0.1.4"]
@@ -6,5 +6,5 @@
 {:dev {:resource-paths ["test/resources"]
        :plugins [[codox "0.10.0"]]
        :codox {:source-paths ["src/clojure"]}
-       :env {:marta-api-key "475ad2ba-5928-4063-9d00-ae06fbb02f3c"
+       :env {:marta-api-key "api-key-here"
              :marta-api-uri "http://developer.itsmarta.com"}}}
